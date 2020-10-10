3f84a50ca901aa8c87cc313de16585774f378b21
RohanDeySarkar <41055662+RohanDeySarkar@users.noreply.github.com>
2020-10-10 11:54:38 +0530

    Updates

---
+++ precache-manifest.c69d61d311a5cb16c69fd11ae1526f18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b1889c162007d7dff1bcb38e5841c9",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "4412ce36b265f715f4ca",
+    "url": "/Covid-19-Tracker/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "fd7dbc2b833207a2ce7f",
+    "url": "/Covid-19-Tracker/static/css/main.b320ee48.chunk.css"
+  },
+  {
+    "revision": "4412ce36b265f715f4ca",
+    "url": "/Covid-19-Tracker/static/js/2.d3ebca0c.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-19-Tracker/static/js/2.d3ebca0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd7dbc2b833207a2ce7f",
+    "url": "/Covid-19-Tracker/static/js/main.68a19de5.chunk.js"
+  },
+  {
+    "revision": "e53de683d501869629f6",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.af13cdb2.js"
+  }
+]);
